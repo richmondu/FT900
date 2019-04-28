87f905466e8526f599688588eadeb5f2150abeb6
richmondu <richmond.umagat@yahoo.com>
2019-04-28 13:53:56 +0800

    RevC: Support multiple Alexa sessions/instances

--- Alexa/Amazon Alexa Client/Includes/avs_config.h
+++ Alexa/Amazon Alexa Client/Includes/avs_config.h
@@ -5,25 +5,26 @@
 
 ///////////////////////////////////////////////////////////////////////////////////
 // Set your device ID
+// Should be in the range of [1, 16]
 ///////////////////////////////////////////////////////////////////////////////////
-#define AVS_CONFIG_DEVICE_ID            0x00000001
+#define AVS_CONFIG_DEVICE_ID            1
 
 
 ///////////////////////////////////////////////////////////////////////////////////
-// Set your server IP address
+// Set if connecting to different Alexa account
 ///////////////////////////////////////////////////////////////////////////////////
+#define AVS_CONFIG_DIFFERENT_ACCOUNT    0
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// Set your server IP address and port
+///////////////////////////////////////////////////////////////////////////////////
+#if (COMMUNICATION_IO==1) || (COMMUNICATION_IO==2)
 #define AVS_CONFIG_SERVER_ADDR          PP_HTONL(LWIP_MAKEU32(192, 168, 100, 12))
-
-
-///////////////////////////////////////////////////////////////////////////////////
-// Choose your sampling rate
-//   SAMPLING_RATE_44100HZ
-//   SAMPLING_RATE_48KHZ
-//   SAMPLING_RATE_32KHZ
-//   SAMPLING_RATE_16KHZ
-//   SAMPLING_RATE_8KHZ
-///////////////////////////////////////////////////////////////////////////////////
-#define AVS_CONFIG_SAMPLING_RATE        SAMPLING_RATE_16KHZ
+#if AVS_CONFIG_DIFFERENT_ACCOUNT
+#define AVS_CONFIG_SERVER_PORT          (11234 + AVS_CONFIG_DEVICE_ID)
+#endif // AVS_CONFIG_DIFFERENT_ACCOUNT
+#endif // (COMMUNICATION_IO==1) || (COMMUNICATION_IO==2)
 
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -39,6 +40,8 @@
 #define AVS_CONFIG_WIFI_SSID            "Virus-infected Wifi"
 #define AVS_CONFIG_WIFI_PASSWORD        "MahirapTandaan"
 #define AVS_CONFIG_WIFI_SECURITY        eWiFiSecurityWPA2
+#elif (COMMUNICATION_IO==3) // RS485
+// TODO
 #endif // (COMMUNICATION_IO==2) // WiFi
 
 
