db56dba96a81f54838e73ef2cd8c641896ac7f21
richmondu <richmond.umagat@yahoo.com>
2019-04-29 17:52:36 +0800

    Add test scripts

---
+++ Alexa/Amazon Alexa Client Simulator/ft900_alexa_simulator_15devices_instances.bat
@@ -0,0 +1,13 @@
+SETLOCAL
+
+set START_ID=2
+set NUM_DEVICES=15
+set SERVER_ADDR=192.168.100.12
+set USE_DIFFERENT_ACCOUNT=1
+
+set /a END_ID=%NUM_DEVICES%+1
+for /l %%x in (%START_ID%, 1, %END_ID%) do (
+   START python.exe ft900_alexa_simulator.py --serveraddr %SERVER_ADDR% --deviceid %%x --usediffacct %USE_DIFFERENT_ACCOUNT%
+)
+
+ENDLOCAL
