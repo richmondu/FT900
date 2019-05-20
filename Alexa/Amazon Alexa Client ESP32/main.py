b5dd104c494dab28371d32d53e9d1381fe0b0e58
richmondu <richmond.umagat@yahoo.com>
2019-05-20 14:34:14 +0800

    Update main.py

--- Alexa/Amazon Alexa Client ESP32/main.py
+++ Alexa/Amazon Alexa Client ESP32/main.py
@@ -1,10 +1,22 @@
-#from alexa.avs_manager import avs_manager
+from alexa.avs_manager import avs_manager
 from iot.mqtt_manager import mqtt_manager
 import esp
 import gc
 import ntptime
 
 
+
+def test_alexa_demo():
+    alexa_manager = avs_manager()
+    alexa_manager.run()
+    #alexa_manager.quit()
+    alexa_manager.wait()
+
+def test_iot_demo():
+    iot_manager = mqtt_manager()
+    iot_manager.run()
+    #iot_manager.quit()
+    iot_manager.wait()
 
 def main():
 
@@ -16,19 +28,9 @@ def main():
     ntptime.settime()
 
 
-    # run managers
-    iot_manager = mqtt_manager()
-    iot_manager.run()
-    #alexa_manager = avs_manager()
-    #alexa_manager.run()
+    test_alexa_demo()
+    #test_iot_demo()
 
-    # trigger managers to quit
-    #alexa_manager.quit()
-    #iot_manager.quit()
-
-    # wait for managers to quit
-    #alexa_manager.wait()
-    iot_manager.wait()
     return
 
 if __name__ == '__main__':
