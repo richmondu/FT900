3152056301e33703469c522d732482dd5f407422
richmondu <richmond.umagat@yahoo.com>
2019-05-20 14:17:29 +0800

    Update main.py to use iot application

--- Alexa/Amazon Alexa Client ESP32/main.py
+++ Alexa/Amazon Alexa Client ESP32/main.py
@@ -1,20 +1,35 @@
-from alexa.avs_manager import avs_manager
+#from alexa.avs_manager import avs_manager
+from iot.mqtt_manager import mqtt_manager
+import esp
+import gc
+import ntptime
 
 
 
 def main():
 
-    # run alexa manager
-    alexa_manager = avs_manager()
-    alexa_manager.run()
+    # set debug
+    esp.osdebug(None)
+    # activate garbage collector
+    gc.collect()
+    # set rtc time
+    ntptime.settime()
 
-    # trigger alexa manager to quit
+
+    # run managers
+    iot_manager = mqtt_manager()
+    iot_manager.run()
+    #alexa_manager = avs_manager()
+    #alexa_manager.run()
+
+    # trigger managers to quit
     #alexa_manager.quit()
+    #iot_manager.quit()
 
-    # wait for alexa manager to finish
-    alexa_manager.wait()
+    # wait for managers to quit
+    #alexa_manager.wait()
+    iot_manager.wait()
     return
-
 
 if __name__ == '__main__':
     main()
