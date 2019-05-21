3fffdc316b9ed8a48844256e4112e140c32302d1
richmondu <richmond.umagat@yahoo.com>
2019-05-21 16:54:23 +0800

    Display system info

--- Alexa/Amazon Alexa Client ESP32/main.py
+++ Alexa/Amazon Alexa Client ESP32/main.py
@@ -1,9 +1,14 @@
-from alexa.avs_manager import avs_manager
-from iot.mqtt_manager import mqtt_manager
 import esp
 import gc
 import ntptime
 import utime
+import micropython
+import machine
+import ubinascii
+import sys
+
+from alexa.avs_manager import avs_manager
+from iot.mqtt_manager import mqtt_manager
 
 
 
@@ -19,9 +24,26 @@ def test_iot_demo():
     #iot_manager.quit()
     iot_manager.wait()
 
-def localtime_to_string(localtime) :
+def get_string(localtime):
     (year, month, day, hour, minute, second, millis, _tzinfo) = localtime
     return "%d-%02d-%02d_%02d:%02d:%02d.%03d" % (year, month, day, hour, minute, second, millis)
+
+def get_system_info():
+    print("\r\n\r\n")
+    print("=======================================================================")
+    print("SYSTEM INFO")
+    print("=======================================================================")
+    print('datetime   :{}'.format(get_string(utime.localtime())))
+    print('platform   :{}'.format(sys.platform))
+    print('version    :{}'.format(sys.version))
+    print('system     :{}-{}'.format(sys.implementation[0], sys.implementation[1]))
+    print('machine_id :0x{}'.format(ubinascii.hexlify(machine.unique_id()).decode().upper()))
+    print('machine_fq :{}'.format(machine.freq()))
+    print('maxsize    :{}'.format(sys.maxsize))
+    print('modules    :{}'.format(sys.modules))
+    micropython.mem_info()
+    micropython.stack_use()
+    print("=======================================================================\r\n")
 
 def main():
 
@@ -29,14 +51,14 @@ def main():
     esp.osdebug(None)
     # activate garbage collector
     gc.collect()
+
     # set rtc time
     try:
         ntptime.settime()
-        # print current time
-        print("\r\n\r\n\r\n")
-        print(localtime_to_string(utime.localtime()))
     except:
         pass
+    # display system info
+    get_system_info()
 
 
     #test_alexa_demo()
